381d326d40e8d442e26d621fe2bb492830373798
HIMANSHU <135032966+HIMANSHUDIKTIYA1@users.noreply.github.com>
2024-07-05 19:56:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const path = require("path");
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on("connection", function(socket){
+    socket.on("send-location", function(data){
+        io.emit("receive-location",{id: socket.id, ...data});
+    });
+    socket.on("disconnect", function(){
+        io.emit("user disconnected");
+    })
+    console.log(" its connected!!");
+})
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.render("index");
+})
+server.listen( process.env.PORT, () => { console.log('Server is running .... '); });
