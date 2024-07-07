ead0fce910673a03f98d4d73fcbacb57cf34060a
HIMANSHU <135032966+HIMANSHUDIKTIYA1@users.noreply.github.com>
2024-07-07 14:07:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,21 +10,26 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
+const users = {};
+
 io.on("connection", function(socket){
     console.log("Client connected:", socket.id);
 
     socket.on("send-location", function(data){
-        console.log("Location received from", socket.id, data);
+        users[socket.id] = { ...data };
         io.emit("receive-location", { id: socket.id, ...data });
     });
 
     socket.on("disconnect", function(){
         console.log("Client disconnected:", socket.id);
+        delete users[socket.id];
         io.emit("user-disconnected", socket.id);
     });
+
+    // Send existing users' locations to the newly connected user
+    socket.emit("existing-users", users);
 });
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.render("index");
 });
@@ -33,4 +38,3 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
