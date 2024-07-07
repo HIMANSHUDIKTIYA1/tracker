d506e74119af99874cc6416725309a387fcc310d
HIMANSHU <135032966+HIMANSHUDIKTIYA1@users.noreply.github.com>
2024-07-07 14:07:34 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -25,11 +25,21 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 const markers = {};
 
+// Handle existing users' locations
+socket.on("existing-users", (users) => {
+  console.log("Existing users:", users);
+  Object.keys(users).forEach(id => {
+    const { latitude, longitude, username } = users[id];
+    const marker = L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup(`<b>${username}</b>`);
+    markers[id] = marker;
+  });
+});
+
 socket.on("receive-location", (data) => {
   console.log("Received location:", data); // Received data log
   const { id, latitude, longitude, username } = data;
-
-  map.setView([latitude, longitude]);
 
   if (markers[id]) {
     markers[id].setLatLng([latitude, longitude]);
@@ -37,15 +47,14 @@ socket.on("receive-location", (data) => {
     const marker = L.marker([latitude, longitude])
       .addTo(map)
       .bindPopup(`<b>${username}</b>`);
-
     markers[id] = marker;
   }
+  map.setView([latitude, longitude]);
 });
 
-socket.on("user-disconnected", (id) => { // Change to match server-side event name
+socket.on("user-disconnected", (id) => {
   if (markers[id]) {
     map.removeLayer(markers[id]);
     delete markers[id];
   }
 });
-
