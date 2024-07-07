eff24b3b4df677b72c03d226530befaddc0ab166
HIMANSHU <135032966+HIMANSHUDIKTIYA1@users.noreply.github.com>
2024-07-07 13:19:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,18 +11,25 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on("connection", function(socket){
+    console.log("Client connected:", socket.id);
+
     socket.on("send-location", function(data){
-        io.emit("receive-location",{id: socket.id, ...data});
+        console.log("Location received from", socket.id, data);
+        io.emit("receive-location", { id: socket.id, ...data });
     });
+
     socket.on("disconnect", function(){
-        io.emit("user disconnected");
-    })
-    console.log(" its connected!!");
-})
+        console.log("Client disconnected:", socket.id);
+        io.emit("user disconnected", socket.id);
+    });
+});
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.render("index");
-})
-const port = process.env.PORT ;
-server.listen( port  , () => { console.log(`Server is running .... ${port}`); });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
